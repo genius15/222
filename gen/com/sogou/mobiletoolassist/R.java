35d6e23251b9286a1be6c98bda03b24f075986d2
genius15 <26304484@163.com>
2014-12-27 23:00:18 +0800

    add button

--- gen/com/sogou/mobiletoolassist/R.java
+++ gen/com/sogou/mobiletoolassist/R.java
@@ -981,7 +981,13 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int btn_next=0x7f020057;
+        public static final int btn_pause=0x7f020058;
+        public static final int btn_play=0x7f020059;
+        public static final int btn_prev=0x7f02005a;
+        public static final int ic_launcher=0x7f02005b;
+        public static final int icon=0x7f02005c;
+        public static final int sing_icon=0x7f02005d;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -995,13 +1001,21 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050025;
+        public static final int addnotify=0x7f050040;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int btn_custom_next=0x7f050049;
+        public static final int btn_custom_play=0x7f050048;
+        public static final int btn_custom_prev=0x7f050047;
+        public static final int btn_show_custom=0x7f05003d;
+        public static final int btn_show_custom_button=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
+        public static final int custom_icon=0x7f050041;
+        public static final int custom_song_icon=0x7f050045;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1017,6 +1031,7 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int ll_custom_button=0x7f050046;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1041,8 +1056,14 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f05003f;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int tv_custom_content=0x7f050044;
+        public static final int tv_custom_song_name=0x7f05004b;
+        public static final int tv_custom_song_singer=0x7f05004a;
+        public static final int tv_custom_time=0x7f050043;
+        public static final int tv_custom_title=0x7f050042;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1097,8 +1118,11 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_assist=0x7f030017;
-        public static final int fragment_assist=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int custom=0x7f030018;
+        public static final int fragment_assist=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int view_custom=0x7f03001b;
+        public static final int view_custom_button=0x7f03001c;
     }
     public static final class menu {
         public static final int assist=0x7f0c0000;
@@ -1145,6 +1169,7 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
+        public static final int addNotify=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
     }
@@ -1152,11 +1177,17 @@ containing a value of this type.
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
+
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
+
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -1175,6 +1206,8 @@ containing a value of this type.
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f0b0084;
+        public static final int NotificationContent=0x7f0b0085;
+        public static final int NotificationTitle=0x7f0b0086;
         /**  Mimic text appearance in select_dialog_item.xml 
          */
         public static final int TextAppearance_AppCompat_Base_CompactMenu_Dialog=0x7f0b0063;
@@ -1362,6 +1395,7 @@ containing a value of this type.
         public static final int Widget_AppCompat_ProgressBar=0x7f0b000a;
         public static final int Widget_AppCompat_ProgressBar_Horizontal=0x7f0b0009;
         public static final int Widget_AppCompat_Spinner_DropDown_ActionBar=0x7f0b0022;
+        public static final int btn_custom_style=0x7f0b0087;
     }
     public static final class styleable {
         /**  ============================================ 
