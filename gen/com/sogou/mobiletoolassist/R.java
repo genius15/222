06bc47e16f689e4cb212c250c788e8bc96558f36
genius15 <26304484@163.com>
2014-12-28 21:54:51 +0800

    其他文件

--- gen/com/sogou/mobiletoolassist/R.java
+++ gen/com/sogou/mobiletoolassist/R.java
@@ -981,15 +981,17 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int btn_next=0x7f020057;
-        public static final int btn_pause=0x7f020058;
-        public static final int btn_play=0x7f020059;
-        public static final int btn_prev=0x7f02005a;
-        public static final int ic_launcher=0x7f02005b;
-        public static final int icon=0x7f02005c;
-        public static final int sing_icon=0x7f02005d;
+        public static final int btn_del=0x7f020057;
+        public static final int btn_next=0x7f020058;
+        public static final int btn_pause=0x7f020059;
+        public static final int btn_play=0x7f02005a;
+        public static final int btn_prev=0x7f02005b;
+        public static final int ic_launcher=0x7f02005c;
+        public static final int icon=0x7f02005d;
+        public static final int sing_icon=0x7f02005e;
     }
     public static final class id {
+        public static final int SelectPath=0x7f05003c;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1003,19 +1005,16 @@ containing a value of this type.
         public static final int action_mode_close_button=0x7f050024;
         public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050025;
-        public static final int addnotify=0x7f050040;
+        public static final int addnotify=0x7f050041;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int btn_custom_next=0x7f050049;
-        public static final int btn_custom_play=0x7f050048;
-        public static final int btn_custom_prev=0x7f050047;
-        public static final int btn_show_custom=0x7f05003d;
-        public static final int btn_show_custom_button=0x7f05003e;
+        public static final int btn_custom_next=0x7f050048;
+        public static final int btn_show_custom=0x7f05003f;
+        public static final int btn_show_custom_button=0x7f050040;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003c;
-        public static final int custom_icon=0x7f050041;
-        public static final int custom_song_icon=0x7f050045;
+        public static final int custom_icon=0x7f050042;
+        public static final int custom_song_icon=0x7f050046;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1031,7 +1030,7 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int ll_custom_button=0x7f050046;
+        public static final int ll_custom_button=0x7f050047;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1039,6 +1038,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int relativeLayout1=0x7f05004b;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1054,16 +1054,17 @@ containing a value of this type.
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
+        public static final int startObserve=0x7f05003e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003f;
+        public static final int textView1=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int tv_custom_content=0x7f050044;
-        public static final int tv_custom_song_name=0x7f05004b;
-        public static final int tv_custom_song_singer=0x7f05004a;
-        public static final int tv_custom_time=0x7f050043;
-        public static final int tv_custom_title=0x7f050042;
+        public static final int tv_custom_content=0x7f050045;
+        public static final int tv_custom_song_name=0x7f05004a;
+        public static final int tv_custom_song_singer=0x7f050049;
+        public static final int tv_custom_time=0x7f050044;
+        public static final int tv_custom_title=0x7f050043;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1128,6 +1129,7 @@ containing a value of this type.
         public static final int assist=0x7f0c0000;
     }
     public static final class string {
+        public static final int ClearBtn=0x7f0a0011;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
