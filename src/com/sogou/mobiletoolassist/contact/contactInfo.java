e4307c32419c563cb85236c6f20837556f0818d9
genius15 <26304484@163.com>
2015-06-18 23:05:16 +0800

    网络请求组员

--- src/com/sogou/mobiletoolassist/contact/contactInfo.java
+++ src/com/sogou/mobiletoolassist/contact/contactInfo.java
@@ -1,24 +1,17 @@
 package com.sogou.mobiletoolassist.contact;
+import com.google.gson.annotations.SerializedName;
 
 public class contactInfo {
-	public String cnname = null;
-	private String mailAddr = null;
-	private String ipv4addr = null;
-	private String id = null;
-	private String groupid = null;
-	public String getMailAddr() {
-		return mailAddr;
-	}
-	public void setMailAddr(String mailAddr) {
-		//TODO check if invalid
-		this.mailAddr = mailAddr;
-	}
-	public String getIpv4addr() {
-		return ipv4addr;
-	}
-	public void setIpv4addr(String ipv4addr) {
-		//TODO check if invalid
-		this.ipv4addr = ipv4addr;
-	}
+	@SerializedName("email")
+	public String email="";
+	
+	@SerializedName("id")
+	public int id=-1;
+	
+	@SerializedName("name")
+	public String name="";
+	
+	@SerializedName("userGroupIds")
+	public String[] userGroupIds=null;
 	
 }
