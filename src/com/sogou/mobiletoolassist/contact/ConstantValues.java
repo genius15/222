aa822bd21a293bac28e12df115db66cf27061f4d
genius15 <26304484@163.com>
2015-07-21 20:55:29 +0800

    Update ConstantValues.java
    清空部分数据

--- src/com/sogou/mobiletoolassist/contact/ConstantValues.java
+++ src/com/sogou/mobiletoolassist/contact/ConstantValues.java
@@ -1,11 +1,11 @@
 package com.sogou.mobiletoolassist.contact;
 
 public class ConstantValues {
-	public static String allids_url = "http://venus.sogou-inc.com/anonymous/call/userInfoQuery.getGroupMemberUserIds(4,true)";
+	public static String allids_url = "";
 	
-	public static String userinfo_url_pre = "http://venus.sogou-inc.com/anonymous/call/userInfoQuery.getUsersByIds";
+	public static String userinfo_url_pre = "";
 	
-	public static String allgroup_ids_url = "http://venus.sogou-inc.com/anonymous/call/userInfoQuery.getSubUserGroupIds(4)";
+	public static String allgroup_ids_url = "";
 	
-	public static String groupinfo_url_pre = "http://venus.sogou-inc.com/anonymous/call/userInfoQuery.getUserGroupsByIds";
+	public static String groupinfo_url_pre = "";
 }
