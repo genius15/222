4595b8052a57cf3175792e289dfe83d155693596
genius15 <26304484@163.com>
2015-09-18 14:44:15 +0800

    6.4.2

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 34,
-	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/1E/54/CgvXElX7rTWAIJlVAGIXuPCP4J4855.apk",
+	"vc": 53,
+	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/1E/54/CgvXElX7soWAGA1GAGIYZk6sQbk308.apk",
 	"changeLog": "1、设置代理前判断是否已经在系统设置过,2、修复低版本一个兼容崩溃"
 }
