0ad567695efa281efe77c54bc34d9a50f7cc88d7
genius15 <26304484@163.com>
2015-09-18 14:34:31 +0800

    6.4.1

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
 	"vc": 34,
-	"dl": "http://nj02all01.baidupcs.com/file/73f68e59418e933920fd97117e9076a4?bkt=p2-nj02-314&fid=1241859326-250528-993871521849531&time=1442555318&sign=FDTAXGERLBH-DCb740ccc5511e5e8fedcff06b081203-%2Bp4LFdAWDzGAsS9jjyzW6wI%2F1fY%3D&to=nj2hb&fm=Nan,B,T,t&sta_dx=6&sta_cs=0&sta_ft=apk&sta_ct=1&fm2=Nanjing02,B,T,t&newver=1&newfm=1&secfm=1&flow_ver=3&pkey=00002ae7a405b132a969d6e3c1dbdde6a226&sl=80871503&expires=8h&rt=pr&r=457751021&mlogid=1334559888&vuk=1241859326&vbdid=3147322615&fin=AssistActivity_6.4.1_52.apk&fn=AssistActivity_6.4.1_52.apk&slt=pm&uta=0&rtype=1&iv=0&isw=0&dp-logid=6043480327362579373&dp-callid=0.1",
+	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/1E/54/CgvXElX7rTWAIJlVAGIXuPCP4J4855.apk",
 	"changeLog": "1、设置代理前判断是否已经在系统设置过,2、修复低版本一个兼容崩溃"
 }
