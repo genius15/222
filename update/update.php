c1fc71b3b383e2b6e535c84c1b412435e1b4419c
genius15 <26304484@163.com>
2015-10-12 14:07:41 +0800

    pingback

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
 	"vc": 64,
-	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/20/2D/CgvXElYbHMSAClliAGWCKa9omCc873.apk",
+	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/20/2D/CgvXElYbTdyAb6rIAGWCTMxzCNE845.apk",
 	"changeLog": "1、minsdk改为9,2、修复几个泄露"
 }
