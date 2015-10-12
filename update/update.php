6ce469131cdc4fdc96395e3fad871d99292bc6f2
genius15 <26304484@163.com>
2015-10-12 10:41:58 +0800

    6.6.1

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 63,
-	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/20/1E/CogLTVYZCW2AHjtPAGV-1XvleKY277.apk",
-	"changeLog": "1、增加输入法测试包的下载功能,2、UI调整，看悬浮窗是不漂酿了,3、线上崩溃修复"
+	"vc": 64,
+	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/20/2D/CgvXElYbHMSAClliAGWCKa9omCc873.apk",
+	"changeLog": "1、minsdk改为9,2、修复几个泄露"
 }
