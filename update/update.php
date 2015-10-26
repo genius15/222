9d7135259d5e8b245a5c886cc9252897a7b6f67b
genius15 <26304484@163.com>
2015-10-26 12:26:35 +0800

    6.7.1升级

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 64,
-	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/20/2D/CgvXElYbTdyAb6rIAGWCTMxzCNE845.apk",
-	"changeLog": "1、minsdk改为9,2、修复几个泄露"
+	"vc": 68,
+	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/21/3D/CogLTVYoVWiANp56AG3ZLAzFTLQ879.apk",
+	"changeLog": "1、悬浮窗新增WIFI开关,2、增加一键拷贝app数据到sd工具,3、工具UI美化"
 }
