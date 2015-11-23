1f0fdcca061d7663e662150cad587bf774c206ef
genius15 <26304484@163.com>
2015-11-23 17:44:38 +0800

    6.8.2升级

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 68,
-	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/21/3D/CogLTVYoVWiANp56AG3ZLAzFTLQ879.apk",
-	"changeLog": "1、悬浮窗新增WIFI开关,2、增加一键拷贝app数据到sd工具,3、工具UI美化"
+	"vc": 74,
+	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/24/0C/CgvXElZNyyqAQWH0AG9JPCGOXfg055.apk",
+	"changeLog": "1、新增crash和anr监控工具,2、修复4.4以下机型进入应用管理页崩溃的问题"
 }
