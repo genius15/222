dc090616babd8bfb8cda5166015ae37f0359f169
genius15 <26304484@163.com>
2015-12-17 18:05:12 +0800

    6.9.2shengji

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 74,
-	"dl": "http://file.mt.sogou.com/download?fileId=desktopqadev/M00/24/0C/CgvXElZNyyqAQWH0AG9JPCGOXfg055.apk",
-	"changeLog": "1、新增crash和anr监控工具,2、修复4.4以下机型进入应用管理页崩溃的问题"
+	"vc": 79,
+	"dl": "http://file.mt.sogou.com/g2/M00/01/9E/CoZa6FZyh5yAX96gADvmCR3Bsoo145.apk?n=DoraemonBox_Sogou_6.9.2_79.apk",
+	"changeLog": "1、新增渠道包验证工具,2、修复5.0以上系统通知icon为白色的bug,3、代理设置可选择两种方案,4、大幅度减少安装包体积,5、修复线上崩溃及ANR"
 }
