b21c3ef312f983b586778a205459efce28b42fe6
genius15 <26304484@163.com>
2015-12-19 15:58:57 +0800

    6.9.4

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 80,
-	"dl": "http://file.mt.sogou.com/g2/M00/01/BF/CoZa6FZ0ATaAKmmlAD0VomqEACE360.apk?n=DoraemonBox_Sogou_6.9.3_80.apk",
-	"changeLog": "1、新增渠道包验证工具,2、修复5.0以上系统通知icon为白色的bug,3、代理设置可选择两种方案,4、大幅度减少安装包体积,5、修复线上崩溃及ANR"
+	"vc": 81,
+	"dl": "http://file.mt.sogou.com/g2/M00/01/CF/CoZa6FZ1DYWAe0O6AD0yImXrkZw778.apk?n=DoraemonBox_Sogou_6.9.4_81.apk",
+	"changeLog": "1、新增渠道包验证工具,2、修复5.0以上系统通知icon为白色的bug,3、代理设置可选择两种方案,4、大幅度减少安装包体积,5、键盘模拟增加密码键盘"
 }
