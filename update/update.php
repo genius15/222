0cfe8032b1da620ff9b538b36b499bc70aef6f43
genius15 <26304484@163.com>
2015-12-29 15:26:50 +0800

    7.0.1

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 84,
-	"dl": "http://file.mt.sogou.com/g2/M00/03/BB/CoZa6FaBQ_mAVV8BAD5gB2s1jJ8674.apk?n=DoraemonBox_Sogou_7.0.0_84.apk",
-	"changeLog": "1、适配6.0动态权限,2、键盘测试可以横屏了,3、打字性能测试随机时y值减小，防止点到收起按钮"
+	"vc": 85,
+	"dl": "http://file.mt.sogou.com/g2/M00/03/D8/CoZsllaCNUmAf3eXAD5gD4mKp00777.apk?n=DoraemonBox_Sogou_7.0.1_85.apk",
+	"changeLog": "1、适配6.0动态权限,2、键盘测试可以横屏了,3、打字性能测试随机时y值减小，防止点到收起按钮,4、修复反馈页面崩溃"
 }
