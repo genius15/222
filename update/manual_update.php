79c3b8b408e1f8876a9a114548edbe07c4b9957a
genius15 <26304484@163.com>
2015-12-30 22:05:36 +0800

    7.1.0

--- update/manual_update.php
+++ update/manual_update.php
@@ -1,5 +1,5 @@
 {
 	"vc": 88,
-	"dl": "http://file.mt.sogou.com/g2/M00/04/23/CoZa6FaD4zGAPv4dAD58nw4J1F0759.apk?n=DoraemonBox_Sogou_7.1.0_88.apk",
+	"dl": "http://file.mt.sogou.com/g2/M00/04/23/CoZsllaD5I-ANEAhAD58TyjAaac755.apk?n=DoraemonBox_Sogou_7.1.0_88.apk",
 	"changeLog": "1、设备信息更完整,2、修复按键响应UI actionbar问题,3、区分手动升级和自动升级"
 }
