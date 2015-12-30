3cac6fb93248c72ffbc2b17c063eeae1d013f5e9
genius15 <26304484@163.com>
2015-12-30 21:58:25 +0800

    7.1.0

---
+++ update/manual_update.php
@@ -0,0 +1,5 @@
+{
+	"vc": 88,
+	"dl": "http://file.mt.sogou.com/g2/M00/04/22/CoZsllaD35WAAGZmAD58ehb349E364.apk?n=DoraemonBox_Sogou_7.1.0_88.apk",
+	"changeLog": "1、设备信息更完整,2、修复按键响应UI actionbar问题,3、区分手动升级和自动升级"
+}
