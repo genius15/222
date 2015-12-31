cba30bbf3b3538e9f1af682c47d92a52bb588de9
genius15 <26304484@163.com>
2015-12-31 11:02:34 +0800

    shengji

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 86,
-	"dl": "http://file.mt.sogou.com/g2/M00/03/DA/CoZsllaCQRmAO02dAD5pfH9k22g985.apk?n=DoraemonBox_Sogou_7.0.2_86.apk",
-	"changeLog": "1、适配6.0动态权限,2、键盘测试可以横屏了,3、打字性能测试随机时y值减小，防止点到收起按钮,4、修复反馈页面崩溃,5、修复FTP功能"
+	"vc": 88,
+	"dl": "http://file.mt.sogou.com/g2/M00/04/23/CoZsllaD5I-ANEAhAD58TyjAaac755.apk?n=DoraemonBox_Sogou_7.1.0_88.apk",
+	"changeLog": "1、设备信息更完整,2、修复按键响应UI actionbar问题,3、区分手动升级和自动升级"
 }
