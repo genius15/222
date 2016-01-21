58afb29aa87a0fecc45022fdb97b781d759dcfda
genius15 <26304484@163.com>
2016-01-21 17:34:29 +0800

    7.5.0_91

--- update/manual_update.php
+++ update/manual_update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 90,
-	"dl": "http://file.mt.sogou.com/g2/M00/07/6D/CoZa6FagV1qALWzDAFU68XVBzQE840.apk?n=DoraemonBox_Sogou_V7.5.0_90.apk",
+	"vc": 91,
+	"dl": "http://file.mt.sogou.com/g2/M00/07/78/CoZa6FagpV2AV_K5AFU6wOfR0Rs123.apk?n=DoraemonBox_Sogou_V7.5.0_91.apk",
 	"changeLog": "1、全新的logo，扁平化,2、小清新的界面,3、新增自定义时间调整功能,4、增加分享给朋友的功能"
 }
