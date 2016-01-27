b763f18d9cf16d3bd5ce50d6baa2c5af7f227a5c
genius15 <26304484@163.com>
2016-01-27 14:06:19 +0800

    new

--- update/manual_update.php
+++ update/manual_update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 91,
-	"dl": "http://file.mt.sogou.com/g2/M00/07/78/CoZa6FagpV2AV_K5AFU6wOfR0Rs123.apk?n=DoraemonBox_Sogou_V7.5.0_91.apk",
-	"changeLog": "1、全新的logo，扁平化,2、小清新的界面,3、新增自定义时间调整功能,4、增加分享给朋友的功能"
+	"vc": 93,
+	"dl": "http://file.mt.sogou.com/g2/M00/07/ED/CoZsllaoVe2AUc8gAFVIyaieVlU648.apk?n=DoraemonBox_Sogou_V7.5.2_93.apk",
+	"changeLog": "1、调整部分工具的UI,2、内存填充新增比例填充的功能,3、性能监控工具状态出错的bug,4、修改root设置代理失效的问题"
 }
