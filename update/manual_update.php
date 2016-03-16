d992375072b12d35b2a2b25d4596d6d2be9c6152
shijiancug <sj_cug@hotmail.com>
2016-03-16 13:21:20 +0800

    Update manual_update.php
    新版本754

--- update/manual_update.php
+++ update/manual_update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 93,
-	"dl": "http://file.mt.sogou.com/g2/M00/07/ED/CoZsllaoVe2AUc8gAFVIyaieVlU648.apk?n=DoraemonBox_Sogou_V7.5.2_93.apk",
-	"changeLog": "1、调整部分工具的UI,2、内存填充新增比例填充的功能,3、性能监控工具状态出错的bug,4、修改root设置代理失效的问题"
+	"vc": 95,
+	"dl": "http://file.mt.sogou.com/g2/M00/0A/70/CoZsllbo61KAEhbRAFtp9IUEuwU973.apk?n=DoraemonBox_Sogou_V7.5.4_95.apk",
+	"changeLog": "1、性能监控工具增加实时数据展示,2、获取系统日志工具增加实时数据展示,3、修复部分工具的bug"
 }
