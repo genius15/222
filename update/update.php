0914ca2803a6fa4e44591ed0b600d138184c7082
shijiancug <sj_cug@hotmail.com>
2016-03-21 17:37:13 +0800

    新版本升级

--- update/update.php
+++ update/update.php
@@ -1,5 +1,5 @@
 {
-	"vc": 80,
-	"dl": "http://file.mt.sogou.com/g2/M00/01/BF/CoZa6FZz_juAWiaWAD0WFctUiCA904.apk?n=DoraemonBox_Sogou_6.9.3_80.apk",
-	"changeLog": "1、新增渠道包验证工具,2、修复5.0以上系统通知icon为白色的bug,3、代理设置可选择两种方案,4、大幅度减少安装包体积,5、修复线上崩溃及ANR"
+	"vc": 95,
+	"dl": "http://file.mt.sogou.com/g2/M00/0A/70/CoZsllbo61KAEhbRAFtp9IUEuwU973.apk?n=DoraemonBox_Sogou_V7.5.4_95.apk",
+	"changeLog": "1、性能监控工具增加实时数据展示,2、获取系统日志工具增加实时数据展示,3、修复部分工具的bug"
 }
